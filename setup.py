801ab7339ae0cf468ac93b9574b7810281f2b4cb
Ramon Saraiva <ramonsaraiva@gmail.com>
2017-01-07 20:14:22 -0200

    Added setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='timy',
+      version='0.1',
+      description='Minimalist measurement of python code time',
+      url='https://github.com/ramonsaraiva/timy',
+      author='Ramon Saraiva',
+      author_email='ramonsaraiva@gmail.com',
+      license='MIT',
+      packages=['timy'],
+      zip_safe=False)
