236b929ceca25b58b04fc4db1aec196153d7b85e
Ramon Saraiva <ramonsaraiva@gmail.com>
2017-09-23 18:54:17 -0300

    Added test for context processor (__enter__ and __exit__)

--- tests/test_timer_cp.py
+++ tests/test_timer_cp.py
@@ -16,3 +16,16 @@ def test_init():
 def test_init_sleeptime():
     timer = Timer(include_sleeptime=False)
     assert timer.time_func == time.process_time
+
+
+@mock.patch('timy.output')
+def test_cp(p_output):
+    _timer = Timer()
+
+    with mock.patch.object(_timer, 'time_func') as p_time_func:
+        p_time_func.return_value = 1
+        with _timer as timer:
+            pass
+        assert _timer.start == 1
+
+    p_output.assert_called_once_with(_timer.ident, '0.000000')
