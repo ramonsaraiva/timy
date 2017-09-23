1e286d03479c748ad2720dce2ef181bd5192a504
Ramon Saraiva <ramonsaraiva@gmail.com>
2017-09-23 14:36:18 -0300

    Added output tests

--- tests/test_output.py
+++ tests/test_output.py
@@ -1,7 +1,42 @@
-import unittest
+import logging
+
+import mock
+
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 from timy import output
+from timy.settings import (
+    timy_config,
+    TrackingMode)
+
+logger = logging.getLogger('timy')
 
 
-def test_output():
-    pass
+@mock.patch('sys.stdout', new_callable=StringIO)
+def test_output_no_tracking(p_stdout):
+    timy_config.tracking = False
+    timy_config.tracking_mode = TrackingMode.PRINTING
+
+    output('ident', 'text')
+    assert p_stdout.getvalue() == ''
+
+
+@mock.patch('sys.stdout', new_callable=StringIO)
+def test_output_print_tracking(p_stdout):
+    timy_config.tracking = True
+    timy_config.tracking_mode = TrackingMode.PRINTING
+
+    output('ident', 'text')
+    assert p_stdout.getvalue() == 'ident text seconds\n'
+
+
+def test_output_logger_tracking():
+    timy_config.tracking = True
+    timy_config.tracking_mode = TrackingMode.LOGGING
+
+    with mock.patch.object(logger, 'info') as p_info:
+        output('ident', 'text')
+        p_info.assert_called_with('ident text seconds')
