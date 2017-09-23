2dd68209528a9e0199c3ffd4268c684085b9486c
Ramon Saraiva <ramonsaraiva@gmail.com>
2017-09-23 19:07:30 -0300

    Added tests for elapsed and track

--- tests/test_timer_cp.py
+++ tests/test_timer_cp.py
@@ -29,3 +29,22 @@ def test_cp(p_output):
         assert _timer.start == 1
 
     p_output.assert_called_once_with(_timer.ident, '0.000000')
+
+
+def test_elapsed():
+    timer = Timer()
+    with mock.patch.object(timer, 'time_func') as p_time_func:
+        p_time_func.return_value = 1
+        elapsed = timer.elapsed
+        assert elapsed == 1
+
+
+@mock.patch('timy.output')
+def test_track(p_output):
+    timer = Timer()
+    with mock.patch(
+            'timy.Timer.elapsed', new_callable=mock.PropertyMock) as p_elapsed:
+        p_elapsed.return_value = 1
+        timer.track()
+        p_output.assert_called_once_with(
+            timer.ident, '(track) 1.000000')
