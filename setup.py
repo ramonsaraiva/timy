3320561abc2d5de54f4ad711286269de8e0f9d50
Ramon Saraiva <ramonsaraiva@gmail.com>
2018-08-06 14:28:27 -0300

    Add long description

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
+import os
 from setuptools import setup
 
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+
 setup(name='timy',
-      version='0.4.0',
+      version='0.4.1',
       description='Minimalist measurement of python code time',
+      long_description=read('README.md'),
       url='https://github.com/ramonsaraiva/timy',
       author='Ramon Saraiva',
       author_email='ramonsaraiva@gmail.com',
